d0fceb46f05cf37d2909096cc9e0aa841638242f
tusihao@hzsuidifu.com <tusihao@hzsuidifu.com>
2021-07-16 17:40:04 +0800

    -

---
+++ c.sh
@@ -0,0 +1,2 @@
+./bin/automatic -host 101.71.36.214 -port 53308 -acc pay -pwd pay1502 -path ./models/roths -d roths -t order_channel,order_payment,order_channel_state
+./bin/automatic -host 101.71.36.214 -port 53308 -acc pay -pwd pay1502 -path ./models/frigate -d frigate -t task,task_info,task_state
